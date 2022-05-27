d5a53e7e4563c9e4249ea6b52335c176eea0a7a7
Sachin Mittalkod <Sachin.Mittalkod@euromonitor.com>
2022-05-27 12:07:59 +0530

    done

--- Angular1/src/app/app.component.ts
+++ Angular1/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular1';
+  message:string='hello'
+  event:string='dude'
+
+  clicked():void{
+    alert("You have submitted")
+  }
 }
--- Angular1/src/app/app.module.ts
+++ Angular1/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './Components/home-page/home-page.component';
 import { CartComponent } from './Components/cart/cart.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +17,14 @@ import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.co
     CartComponent,
     AboutUsComponent,
     PagenotfoundComponent
+   
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
