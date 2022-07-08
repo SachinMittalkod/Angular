3c953415bc08c063417b7f20a4da14604a198046
Sachin Mittalkod <Sachin.Mittalkod@euromonitor.com>
2022-07-08 16:41:29 +0530

    Done

--- PostAssessment/src/app/Component/availabilitybooks/availabilitybooks.component.ts
+++ PostAssessment/src/app/Component/availabilitybooks/availabilitybooks.component.ts
@@ -10,9 +10,12 @@ import { BookService } from 'src/app/service/book.service';
 export class AvailabilitybooksComponent implements OnInit {
   term:string="";
   mydata:Bookdata[]=[];
+  today = new Date();
+  tomorrow = new Date(this.today.getTime() + (24 * 60 * 60 * 1000));
+  // specificdate= this.tomorrow.getDate()+1;
   constructor(private service:BookService) { }
   recdata:Bookdata[]=[];
-
+  // date: this.date= this.tomorrow.setDate(this.tomorrow.getDate()+1)
   ngOnInit(): void {
   this.service.getadminbook().subscribe(resp=>{
     this.mydata=resp;
